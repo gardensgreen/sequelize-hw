52b52505a3c7cf7c6a028f2816bff51f6079eecd
Daniel TIllero <danieltillero67@gmail.com>
2020-10-15 10:24:03 -0400

    Querying with sequelize

--- index.js
+++ index.js
@@ -46,6 +46,80 @@ async function main() {
     // Destroy the Cat record with id #3.
     await Cat.destroy({ where: { id: 3 } });
 
+    // `findAll` asks to retrieve _ALL_ THE CATS!!  An array of `Cat`
+    // objects will be returned.
+    const cats = await Cat.findAll();
+
+    // Fetch all cats named Markov.
+    const cats = await Cat.findAll({
+        where: {
+            firstName: "Markov",
+        },
+    });
+
+    // Fetch all cats named either Markov or Curie.
+    const cats = await Cat.findAll({
+        where: {
+            firstName: ["Markov", "Curie"],
+        },
+    });
+
+    const cats = await Cat.findAll({
+        where: {
+            firstName: {
+                // Op.ne means the "not equal" operator.
+                [Op.ne]: "Markov",
+            },
+        },
+    });
+
+    // fetch cats with name != Markov AND age = 4.
+    const cats = await Cat.findAll({
+        where: {
+            firstName: {
+                [Op.ne]: "Markov",
+            },
+            age: 4,
+        },
+    });
+
+    // fetch cats with name == Markov OR age = 4.
+    const cats = await Cat.findAll({
+        where: {
+            [Op.or]: [{ firstName: "Markov" }, { age: 4 }],
+        },
+    });
+
+    // Fetch all cats whose age is > 4.
+    const cats = await Cat.findAll({
+        where: {
+            age: { [Op.gt]: 4 },
+        },
+    });
+
+    //Ordering results
+    const cats = await Cat.findAll({
+        order: [["age", "DESC"]],
+    });
+
+    //Limiting Results
+    const cats = await Cat.findAll({
+        order: [["age", "DESC"]],
+        limit: 1,
+    });
+
+    //When we want a max of one result and not in an array!
+    const cat = await Cat.findOne({
+        order: [["age", "DESC"]],
+    });
+
+    // Try to find a non-existant cat with find one will return null instead of an empty array.
+    const cat = await Cat.findOne({
+        where: {
+            firstName: "Franklin Delano Catsevelt",
+        },
+    });
+
     await sequelize.close();
 }
 
