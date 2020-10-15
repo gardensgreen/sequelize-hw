2d4f409ba2027d3f5b47d27ed39efa867ed525b2
Daniel TIllero <danieltillero67@gmail.com>
2020-10-15 10:14:22 -0400

    Crud operations using sequelize

--- index.js
+++ index.js
@@ -11,8 +11,41 @@ async function main() {
         return;
     }
 
+    // Constructs an instance of the JavaScript `Cat` class. **Does not
+    // save anything to the database yet**. Attributes are passed in as a
+    // POJO.
+    const cat = Cat.build({
+        firstName: "Markov",
+        specialSkill: "sleeping",
+        age: 5,
+    });
+    // This actually creates a new `Cats` record in the database. We must
+    // wait for this asynchronous operation to succeed.
+    await cat.save();
+
+    //Fetch the cat with id #1
     const cat = await Cat.findByPK(1);
     console.log(cat.toJSON());
+
+    // The Cat object is modified, but the corresponding record in the
+    // database is *not* yet changed at all.
+    cat.specialSkill = "super deep sleeping";
+    // Only by calling `save` will the data be saved.
+    await cat.save();
+
+    // Remove the Markov record.
+    await cat.destroy();
+
+    //Builds and saves in one step
+    const cat = await Cat.create({
+        firstName: "Curie",
+        specialSkill: "jumping",
+        age: 4,
+    });
+
+    // Destroy the Cat record with id #3.
+    await Cat.destroy({ where: { id: 3 } });
+
     await sequelize.close();
 }
 
