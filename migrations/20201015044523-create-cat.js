ef8f782a86a5390d6c85357ff6f259ea96c34c35
Daniel TIllero <danieltillero67@gmail.com>
2020-10-15 10:06:47 -0400

    run sequelize migrations

--- migrations/20201015044523-create-cat.js
+++ migrations/20201015044523-create-cat.js
@@ -1,30 +1,33 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Cats', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      specialSkill: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Cats');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("Cats", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            firstName: {
+                type: Sequelize.STRING,
+            },
+            specialSkill: {
+                type: Sequelize.STRING,
+            },
+            age: {
+                type: Sequelize.INTEGER,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("Cats");
+    },
 };
